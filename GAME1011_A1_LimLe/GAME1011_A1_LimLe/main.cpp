cdea788323df5e0fd7cf1281f0ee3f69dde91928
Nitsua but backwards <74437061+ItsNitsua@users.noreply.github.com>
2021-02-10 18:45:59 -0500

    Added Main

---
+++ GAME1011_A1_LimLe/GAME1011_A1_LimLe/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+	int participantsSize;
+
+	cout << "How many do you want in your student survey?" << endl;
+	cin >> participantsSize;
+}
